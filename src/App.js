20a40636292f3f9096fca666b4275e2fc743d2df
I an <ydh8485@gmail.com>
2021-04-07 10:20:16 +0900

    Provider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ColorBox from "./components/ColorBox";
+import ColorContext from "./contexts/color";
 
 function App() {
   return (
-    <div>
-      <ColorBox />
-    </div>
+    // Provider 사용시 기본값을 명시해 주어야 한다 = value
+    <ColorContext.Provider value={{ color: "red" }}>
+      <div>
+        <ColorBox />
+      </div>
+    </ColorContext.Provider>
   );
 }
 
--- src/contexts/color.js
+++ src/contexts/color.js
@@ -1,5 +1,5 @@
 import { createContext } from "react";
-
+// createContext의 파라미터 : Provider를 사용하지 않을 떄만 사용되는 기본값
 const ColorContext = createContext({ color: "black" });
 
 export default ColorContext;
